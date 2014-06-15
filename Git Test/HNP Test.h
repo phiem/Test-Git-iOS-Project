f0cc5867a7a1512692a6bc12a14db8d76abefa8f
Phiem Huynh <phiemhuynh@phiems-imac.home>
2014-06-15 10:57:03 -0400

    added a test class

---
+++ Git Test/HNP Test.h
@@ -0,0 +1,13 @@
+//
+//  HNP Test.h
+//  Git Test
+//
+//  Created by Phiem Huynh on 6/15/14.
+//  Copyright (c) 2014 Phiem Huynh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HNP_Test : NSObject
+
+@end
---
+++ Git Test/HNP Test.m
@@ -0,0 +1,13 @@
+//
+//  HNP Test.m
+//  Git Test
+//
+//  Created by Phiem Huynh on 6/15/14.
+//  Copyright (c) 2014 Phiem Huynh. All rights reserved.
+//
+
+#import "HNP Test.h"
+
+@implementation HNP_Test
+
+@end
